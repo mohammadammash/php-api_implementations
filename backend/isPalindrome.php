c86cb2174d8bcd1f7cd946b2b4a59f88cc36f1e5
mohammadammash <mohammadammash31@gmail.com>
2022-09-13 07:12:00 +0300

    Adding 'backend/isPalindrome.php' to handle GET request to this URL and check if it palindrome or not

---
+++ backend/isPalindrome.php
@@ -0,0 +1,24 @@
+<!-- php page to handle GET request to this URL -->
+<?php
+function isPalindrome($str)
+{
+    $l = 0;
+    $r = strlen($str) - 1;
+    while ($l < $r) {
+        if ($str[$l] != $str[$r]) {
+            return False;
+        }
+        $l = $l + 1;
+        $r = $r - 1;
+    }
+    return True;
+}
+
+
+$text = $_GET['text'];
+if (isPalindrome(($text))) {
+    echo "${text} is Palindrome :)";
+} else {
+    echo "${text} is not Palindrome :(";
+}
+?>
